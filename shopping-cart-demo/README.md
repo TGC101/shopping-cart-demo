e801144d550a2ca1f2d02a49e3ddcab616bf1e5b
tibame tgc101 <tgc101@tibame.ga>
2022-07-07 20:22:34 +0800

    update

---
+++ shopping-cart-demo/README.md
@@ -0,0 +1,35 @@
+# 使用方式
+
+
+請直接修改 js 資料夾內的 products.js 內的陣列元素，要增減多少自行決定...
+
+
+```
+const products = [
+    {
+        id: '1',
+        title: '產品一',
+        price: 10,
+        img: 'https://picsum.photos/id/999/1200/600',
+        tags: ['生活用品', '工具'],
+        isAvailable: true
+    },
+    {
+        id: '2',
+        title: '產品二',
+        price: 60,
+        img: 'https://picsum.photos/id/1070/1200/600',
+        tags: ['藥妝'],
+        isAvailable: true
+    },
+    ....
+    // 以此類推
+];
+```
+
+圖片都是透過 https://picsum.photos  上產生假圖 ....
+
+
+## DEMO
+
+![](https://i.imgur.com/4k0fs1i.jpg)
---
+++ shopping-cart-demo/js/products.js
@@ -0,0 +1,121 @@
+const products = [
+    {
+        id: '1',
+        title: '產品一',
+        price: 10,
+        img: 'https://picsum.photos/id/999/1200/600',
+        tags: ['生活用品', '工具'],
+        isAvailable: true
+    },
+    {
+        id: '2',
+        title: '產品二',
+        price: 60,
+        img: 'https://picsum.photos/id/1070/1200/600',
+        tags: ['藥妝'],
+        isAvailable: true
+    },
+    {
+        id: '3',
+        title: '產品三',
+        price: 180,
+        img: 'https://picsum.photos/id/1071/1200/600',
+        tags: ['食品'],
+        isAvailable: true
+    },
+    {
+        id: '4',
+        title: '產品四',
+        price: 220,
+        img: 'https://picsum.photos/id/1072/1200/600',
+        tags: ['生活用品'],
+        isAvailable: true
+    },
+    {
+        id: '5',
+        title: '產品五',
+        price: 360,
+        img: 'https://picsum.photos/id/1073/1200/600',
+        tags: ['工具'],
+        isAvailable: true
+    },
+    {
+        id: '6',
+        title: '產品六',
+        price: 360,
+        img: 'https://picsum.photos/id/1074/1200/600',
+        tags: ['食品'],
+        isAvailable: true
+    },
+    {
+        id: '7',
+        title: '產品七',
+        price: 400,
+        img: 'https://picsum.photos/id/1075/1200/600',
+        tags: ['生活用品', '工具'],
+        isAvailable: true
+    },
+    {
+        id: '8',
+        title: '產品八',
+        price: 450,
+        img: 'https://picsum.photos/id/1076/1200/600',
+        tags: ['生活用品', '工具'],
+        isAvailable: true
+    },
+    {
+        id: '9',
+        title: '產品九',
+        price: 5200,
+        img: 'https://picsum.photos/id/1077/1200/600',
+        tags: ['藥妝'],
+        isAvailable: true
+    },
+    {
+        id: '10',
+        title: '產品十',
+        price: 5200,
+        img: 'https://picsum.photos/id/1077/1200/600',
+        tags: ['生活用品'],
+        isAvailable: true
+    },
+    {
+        id: '11',
+        title: '產品十一',
+        price: 200,
+        img: 'https://picsum.photos/id/1077/1200/600',
+        tags: ['藥妝'],
+        isAvailable: true
+    }, {
+        id: '12',
+        title: '產品十二',
+        price: 360,
+        img: 'https://picsum.photos/id/1073/1200/600',
+        tags: ['工具'],
+        isAvailable: true
+    },
+    {
+        id: '13',
+        title: '產品十三',
+        price: 360,
+        img: 'https://picsum.photos/id/1074/1200/600',
+        tags: ['食品'],
+        isAvailable: true
+    },
+    {
+        id: '14',
+        title: '產品十四',
+        price: 400,
+        img: 'https://picsum.photos/id/1075/1200/600',
+        tags: ['生活用品', '工具'],
+        isAvailable: true
+    },
+    {
+        id: '15',
+        title: '產品十五',
+        price: 450,
+        img: 'https://picsum.photos/id/1076/1200/600',
+        tags: ['生活用品', '工具'],
+        isAvailable: true
+    },
+];
